53f82b17727264f05b0e8f2cbdc9e8f1be487ecd
nitesh2307 <nitesh2307@gmail.com>
2023-03-05 12:16:46 +0530

    test file update
    test file update desc

--- Test-File.py
+++ Test-File.py
@@ -1,1 +1,26 @@
-Test file
+f = open("day3.txt", "r")
+d = {}
+sum = 0
+with open("day3lookup.txt") as g:
+    for line in g:
+        (key, val) = line.split()
+        d[key] = int(val)
+#print (d)
+
+for x in f:
+    y = x.strip()
+  #  print(len(y)) 
+    first_half = y[:len(y)//2]
+    second_half = y[len(y)//2:]
+    #print(y + " ----- " + first_half + "  +  " + second_half)
+    for i in first_half:
+        if second_half.find(i) != -1:
+            sum = sum + int(d[i])
+            break
+        else:
+            continue
+print("Answer is below Kushagr")
+print(sum)
+
+
+
